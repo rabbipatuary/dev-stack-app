14b022a3db7852dd05a6c6cfa157a1db4272cf32
Rabbi Patuary <omarpaturay@gmail.com>
2026-09-14 10:37:14 +0600

    nav and banner section end

---
+++ src/component/Banner.tsx
@@ -0,0 +1,40 @@
+
+import banner from '../assets/hero.png'
+const Banner = () => {
+    return (
+        <div className="flex justify-between items-center container mx-auto mt-9">
+
+    <div className="space-y-6">
+
+        <h1 className="text-[40px] font-bold">
+            Build Your Ideal <br />
+            <span className="bg-linear-to-r from-orange-500 via-pink-500 to-violet-500 bg-clip-text text-transparent">
+                Development Stack
+            </span>
+        </h1>
+
+        <p>
+            Explore frontend, backend, database, and tooling options,
+            <br />
+            compare them side by side, and put together the stack that fits your
+            <br />
+            next project.
+        </p>
+
+        <button className="py-2 px-4 border rounded-2xl mr-4 mt-4 text-white bg-linear-to-r from-orange-500 via-pink-500 to-violet-500">
+            Explore Technologies
+        </button>
+
+        <button className="py-2 px-4 border rounded-2xl">
+            Learn More
+        </button>
+
+    </div>
+
+    <img src={banner} alt="" />
+
+</div>
+    );
+};
+
+export default Banner;
---
+++ src/component/Nav.tsx
@@ -0,0 +1,32 @@
+import logo from '../assets/logo-text.png'
+const Nav = () => {
+    return (
+        <div className="flex justify-between items-center container mx-auto">
+
+    <div>
+        <img src={logo} alt="logo" />
+    </div>
+
+    <ul className="flex gap-4">
+        <li><a href=" ">Home</a></li>
+        <li><a href=" ">Technologies</a></li>
+        <li><a href=" ">Projects</a></li>
+        <li><a href=" ">About</a></li>
+        <li><a href=" ">Contact</a></li>
+    </ul>
+
+    <div className="">
+        <button className="py-2 px-4 border rounded-2xl m-4">
+            Sign In
+        </button>
+
+        <button className="py-2 px-4 border bg-pink-500 text-white rounded-[20px]">
+            Sign Up
+        </button>
+    </div>
+
+</div>
+    );
+};
+
+export default Nav;
