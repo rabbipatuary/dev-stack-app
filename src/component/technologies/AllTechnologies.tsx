a4f48f630e5ff962a400d0693d2397fdeb67fb81
Rabbi Patuary <omarpaturay@gmail.com>
2026-09-14 22:09:47 +0600

    last

--- src/component/technologies/AllTechnologies.tsx
+++ src/component/technologies/AllTechnologies.tsx
@@ -10,7 +10,7 @@ const AllTechnologies = ({ teches }: { teches: Itechnologies[] }) => {
   return (
     <div className="grid grid-cols-12 container mx-auto gap-12 justify-center">
 
-      <div className="grid grid-cols-3 col-span-9 gap-4 p-4">
+      <div className="grid grid-cols-3 col-span-9 gap-4">
         {teches.map((tech) => {
           return (
             <TechnologyCard
